7d550d0816230b93afb82b17effe878af55cbbcc
Daniel Fidalgo <daniel@montra.com>
2024-04-02 10:56:18 -0500

    new-scripts - missing adjusts

--- copy_branch_name.sh
+++ copy_branch_name.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-BRANCH_NAME=$(git symbolic-ref --short HEAD)
-pbcopy $BRANCH_NAME
+git symbolic-ref --short HEAD | pbcopy
--- merge_to_dev.sh
+++ merge_to_dev.sh
@@ -4,6 +4,6 @@ git branch -D development
 git fetch origin development
 git checkout development
 git pull 
-git merge $BRANCH_TO_MERGE
+git merge $BRANCH_TO_MERGE --no-edit
 git push 
 git checkout $BRANCH_TO_MERGE
